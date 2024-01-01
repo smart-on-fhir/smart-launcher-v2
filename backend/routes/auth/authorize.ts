6ff90494b05c9854bc32948fcf13fd349e8fd9a9
yashpmohod <117890382+yashpmohod@users.noreply.github.com>
2024-01-01 14:54:13 +0530

    RT-1023:(smart-launcher-v2) No encounter in launch context (#15)

--- backend/routes/auth/authorize.ts
+++ backend/routes/auth/authorize.ts
@@ -586,7 +586,10 @@ export default class AuthorizeHandler {
     }
 
     private async getFirstEncounterId(): Promise<string | undefined> {
-        const fhirServer = getFhirServerBaseUrl(this.request as any);
+        let fhirServer = getFhirServerBaseUrl(this.request as any);
+        if (!fhirServer.endsWith('/')) {
+            fhirServer = fhirServer + '/';
+        } 
         const url = new URL(`Encounter/?_count=1&_sort:desc=date`, fhirServer)
         url.searchParams.set("patient", this.launchOptions.patient.get(0)!)
         const res = await fetch(url)
