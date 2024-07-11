070e9d1f9b5b29220e48962b674d9cdb7622c45d
Sean Fong <sean.fong@csiro.au>
2024-07-11 13:50:44 +0930

    Remove frontend code - we are not using it

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React    from 'react'
 import ReactDOM from 'react-dom/client'
-import App      from './components/App/App'
 
 
 const root = ReactDOM.createRoot(
@@ -9,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <>Frontend has been disabled</>
   </React.StrictMode>
 );
